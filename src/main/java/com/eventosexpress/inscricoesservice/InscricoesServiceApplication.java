31e819a5290d765abad771410316cf49174ae4c0
Otavio Oliveira <otavio.oliveira@al.infnet.edu.br>
2026-08-11 12:40:30 +0100

    chore: cria estrutura inicial do microsserviço de inscrições

---
+++ src/main/java/com/eventosexpress/inscricoesservice/InscricoesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eventosexpress.inscricoesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscricoesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InscricoesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventosexpress/inscricoesservice/InscricoesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventosexpress.inscricoesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscricoesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
